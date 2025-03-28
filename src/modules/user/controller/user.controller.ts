e9f57424718165d9c73034bbcb57f8d1fd250876
Phát Hồ <phatho@Phats-MacBook-Pro.local>
2025-03-28 21:52:11 +0700

    refactor response and kafka service

--- src/modules/user/controller/user.controller.ts
+++ src/modules/user/controller/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, UsePipes, ValidationPipe, Param, Logger, Res, Put, Delete, HttpStatus, HttpCode, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Body, UsePipes, ValidationPipe, Param, Logger, Res, Put, Delete, HttpStatus, HttpCode, UseGuards, UseInterceptors, HttpException } from '@nestjs/common';
 import { UserService } from '../service/user.service';
 import { CreateUserDTO } from '../dto/createUser.dto';
 import { GetUser } from '../dto/getUser.dto';
@@ -21,15 +21,22 @@ export class UserController {
   @HttpCode(HttpStatus.CREATED)
   @Post()
   @UsePipes(new ValidationPipe())
-  async createUser(@Body() createUserDto: CreateUserDTO): Promise<CreateUserDTO> {
+  async createUser(
+    @Body() createUserDto: CreateUserDTO,
+    @Res() res
+  ): Promise<CreateUserDTO | HttpException> {
     try {
       this.logger.log(`Creating User`);
       const user = await this.userService.createUser(createUserDto);
       this.logger.log(`Created User`);
-      return user;
+      return res.status(HttpStatus.CREATED).json({
+        data: user,
+        message: 'User created successfully',
+        status: HttpStatus.CREATED
+      });
     } catch (error) {
       this.logger.error(`Error creating user: ${error.message}`);
-      throw new Error(`Error creating user: ${error.message}`);
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
   }
 
@@ -37,45 +44,64 @@ export class UserController {
   @UseGuards(AuthGuard, RoleGuard)
   @Roles(UserRole.ADMIN)
   @Get('all')
-  async getUsers(): Promise<GetUser[]> {
+  async getUsers(@Res() res): Promise<GetUser[] | HttpException> {
     try{
       this.logger.log(`Getting all users`);
       const users = await this.userService.getUsers();
       this.logger.log(`Got all users`);
-      return users;
+      return res.status(HttpStatus.OK).json({
+        data: users,
+        message: 'Get Users created successfully',
+        status: HttpStatus.OK
+      });
     }catch(error){
       this.logger.error(`Error getting all users: ${error.message}`);
-      throw new Error(`Error creating user: ${error.message}`);
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
   }
 
   @HttpCode(HttpStatus.OK)
   @UseGuards(AuthGuard)
   @Get(':id')
-  async getUserById(@Param('id') userId: string): Promise<GetUser | null> {
+  async getUserById(
+    @Param('id') userId: string,
+    @Res() res
+  ): Promise<GetUser | HttpException> {
     try{
       this.logger.log(`Getting user by id`);
       const user = await this.userService.getUserById(userId);
       this.logger.log(`Got user by id`);
-      return user;
+
+      return res.status(HttpStatus.OK).json({
+        data: user,
+        message: 'Get User successfully',
+        status: HttpStatus.OK
+      });
     }catch(error){
       this.logger.error(`Error getting user by id: ${error.message}`);
-      throw new Error(`Error creating user: ${error.message}`);
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
   }
 
   @HttpCode(HttpStatus.OK)
   @UseGuards(AuthGuard)
   @Get('email/:email') 
-  async getUserByEmail(@Param('email') email: string): Promise<GetUser | null> {
+  async getUserByEmail(
+    @Param('email') email: string,
+    @Res() res
+  ): Promise<GetUser | HttpException> {
     try{
       this.logger.log(`Getting user by email`);
       const user = await this.userService.getUserByEmail(email);
       this.logger.log(`Got user by email`);
-      return user;
+      return res.status(HttpStatus.OK).json({
+        data: user,
+        message: 'Get User successfully',
+        status: HttpStatus.OK
+      });
     }catch(error) {
       this.logger.error(`Error getting user by email: ${error.message}`);
-      throw new Error(`Error creating user: ${error.message}`);
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
   }
 
@@ -83,28 +109,42 @@ export class UserController {
   // @Roles('admin')
   @UseGuards(AuthGuard)
   @Put(':id')
-  async updateUser(@Param('id') userId: string, @Body() createUserDto: Partial<CreateUserDTO>): Promise<CreateUserDTO> {
+  async updateUser(
+    @Param('id') userId: string, 
+    @Body() createUserDto: Partial<CreateUserDTO>,
+    @Res() res
+  ): Promise<CreateUserDTO | HttpException> {
     try{
       this.logger.log(`Updating user`);
       const user = await this.userService.updateUser(userId, createUserDto);
-      this.logger.log(`Updated user`);
-      return user;
+      return res.status(HttpStatus.OK).json({
+        data: user,
+        message: 'User updated successfully',
+        status: HttpStatus.OK
+      });
     }catch(error){
       this.logger.error(`Error updating user: ${error.message}`);
-      throw new Error(`Error updating user: ${error.message}`);
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
   }
 
   @Delete(':id')
-  async deleteUser(@Param('id') userId: string): Promise<boolean> {
+  async deleteUser(
+    @Param('id') userId: string,
+    @Res() res
+  ): Promise<boolean | HttpException> {
     try{
       this.logger.log(`Deleting user`);
       const user = await this.userService.deleteUser(userId);
       this.logger.log(`Deleted user`);
-      return user;
+      return res.status(HttpStatus.OK).json({
+        data: user,
+        message: 'User deleted successfully',
+        status: HttpStatus.OK
+      });
     }catch(error){
       this.logger.error(`Error deleting user: ${error.message}`);
-      throw new Error(`Error deleting user: ${error.message}`);
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
   }
 }
